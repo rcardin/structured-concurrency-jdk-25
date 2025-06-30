b807bd274ab68d4a66bd5f633a2c5de7f8517e59
rcardin <riccardo.cardin@gmail.com>
2025-06-30 17:52:32 +0200

    feat: WIP Started the example for `AnySuccessful` joiner

--- src/main/java/in/rcard/sc/Main.java
+++ src/main/java/in/rcard/sc/Main.java
@@ -2,7 +2,10 @@ package in.rcard.sc;
 
 import java.net.URI;
 import java.time.Duration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.concurrent.StructuredTaskScope;
 import java.util.concurrent.StructuredTaskScope.Joiner;
 import org.slf4j.Logger;
@@ -114,6 +117,41 @@ public class Main {
 
         return new GitHubUser(user.get(), repositories.get());
       }
+    }
+  }
+
+  static class FindRepositoriesByUserIdCache implements FindRepositoriesByUserIdPort {
+
+    private final Map<UserId, List<Repository>> cache = new HashMap<>();
+
+    public FindRepositoriesByUserIdCache() {
+      cache.put(
+              new UserId(42L),
+              List.of(
+                      new Repository(
+                              "rockthejvm.github.io",
+                              Visibility.PUBLIC,
+                              URI.create("https://github.com/rockthejvm/rockthejvm.github.io"))));
+    }
+
+    @Override
+    public List<Repository> findRepositories(UserId userId) throws InterruptedException {
+      // Simulates access to a distributed cache (Redis?)
+      delay(Duration.ofMillis(100L));
+      final List<Repository> repositories = cache.get(userId);
+      if (repositories == null) {
+        LOGGER.info("No cached repositories found for user with id '{}'", userId);
+        throw new NoSuchElementException(
+                "No cached repositories found for user with id '%s'".formatted(userId));
+      }
+      return repositories;
+    }
+
+    public void addToCache(UserId userId, List<Repository> repositories)
+            throws InterruptedException {
+      // Simulates access to a distributed cache (Redis?)
+      delay(Duration.ofMillis(100L));
+      cache.put(userId, repositories);
     }
   }
 
